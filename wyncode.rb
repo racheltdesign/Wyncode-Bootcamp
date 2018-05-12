c4ff91a41ec7451ac62aaf92d4ed77ebc3a14545
Rachel Thompson <thompr@pba.edu>
2018-05-12 14:30:41 -0400

    Update wyncode.rb

--- wyncode.rb
+++ wyncode.rb
@@ -2,6 +2,11 @@ puts "What is the bill?"
 bill = gets.chomp.to_i
 puts "How much tip to give?(%)"
 tip_percent = gets.chomp.to_i
+
+if tip_percent <= 15
+  puts "Cheapo..."
+end
+
 tip = bill * (tip_percent / 100.0)
 
 total = bill + tip
